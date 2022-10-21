b0105a6b69d028d36834142bee7016d4e69ff45e
Android Studio <devandroid@ort>
2022-10-21 12:13:20 -0300

    camera working

--- Components/Views/CambioFondoView.jsx
+++ Components/Views/CambioFondoView.jsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
-import React, { useState} from 'react'
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Dimensions } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import * as ImagePicker from 'expo-image-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Camera, CameraType, takePictureA } from 'expo-camera';
 
 const CambioFondoView = () => {
   const [image, setImage] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [camaraOpen, setCamaraOpen] = useState(false)
+  const [camera, setCamera] = useState(null)
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  const abrirCamara = () => {
+    requestPermission() 
+    setCamaraOpen(true)
+  }
+
+  const cerrarCamara = () => {
+    setCamaraOpen(false)
+  }
+  const takePicture = async () => {
+    if(camera){
+      const dataPic = await camera.takePictureAsync(null)
+      console.log(dataPic);
+      setImage(dataPic.uri)
+      try {
+        await AsyncStorage.setItem('bg-image', dataPic.uri)
+        setCamaraOpen(false)
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -19,25 +50,82 @@ const CambioFondoView = () => {
       setImage(result.uri);
       try {
         await AsyncStorage.setItem('bg-image', result.uri)
+        setCamaraOpen(false)
       } catch (e) {
         console.log(e);
       }
     }
   };
 
-  return (
-    <View style={{justifyContent: 'center', }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {
-        image && 
-        <View style={{alignItems: 'center', marginTop: 30}}>
-          <Image source={{ uri: image }} style={{ width: 180, height: 340 }} />
-        </View>
-      }
+  return !camaraOpen ? (
+    <View style={{justifyContent: 'center'}}>
+      <Button title='Elegir Fondo' onPress={abrirCamara}/>
+      <Image style={styles.image} source={{uri: image}} resizeMode="cover"/>
     </View>
+  ) : (
+      <Camera 
+        ratio='16:9' 
+        style={styles.camera} 
+        type={type}
+        ref={ref => setCamera(ref)}
+        >
+        <TouchableOpacity style={[styles.buttonCam, styles.buttonCloseCam]} onPress={cerrarCamara}>
+          <Text style={styles.icon}>❌</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.buttonCam, styles.buttonCaptura]} onPress={takePicture}>
+          <Text style={styles.icon}>📸</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.buttonCam, styles.buttonFiles]} onPress={pickImage}>
+          <Text style={styles.icon}>🗃</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.buttonCam, styles.buttonToggleCam]} onPress={toggleCameraType}>
+          <Text style={styles.icon}>↩</Text>
+        </TouchableOpacity>
+      </Camera>
   )
 }
 
 export default CambioFondoView
 
-const styles = StyleSheet.create({})
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  camera: {
+    width: windowWidth,
+    height: windowHeight,
+  },
+  buttonCam: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute'
+  },
+  buttonFiles: {
+    right: 30,
+    top: 30
+  },
+  buttonCaptura: {
+    left: windowWidth*.5 - 30,
+    bottom: 80
+  },
+  buttonCloseCam: {
+    top: 30,
+    left: 30
+  },
+  buttonToggleCam: {
+    bottom: 80,
+    right: 30
+  },
+
+  icon: {
+    fontSize: 28
+  },
+  image: {
+    width: 280,
+    height: 340,
+  }
+})
